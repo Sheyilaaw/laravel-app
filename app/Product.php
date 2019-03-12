750d57bb87dda0556ad811f5e8bf6617b5cf2b00
Sheyilaaw <sheyilaaw98@gmail.com>
2019-03-12 13:13:24 +0100

    Added model for Product

---
+++ app/Product.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oluwaseyi
+ * Date: 12/03/2019
+ * Time: 1:08 PM
+ */
+
+namespace App;
+
+
+class Product
+{
+    protected $fillable = [
+        'name', 'quantity', 'price',
+    ];
+}
